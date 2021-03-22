27101a8f3b0bbeecd2af1bfec099ae8f6dc6cc33
Vinicius Paulino <vinaspsy@gmail.com>
2021-03-22 14:46:11 -0300

    First commit

---
+++ src/utils/load-posts.js
@@ -0,0 +1,15 @@
+export const loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photoResponse]);
+
+    const postJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postJson.map((post, index) => {
+        return { ...post, cover: photosJson[index].url }
+    });
+
+    return postsAndPhotos;
+}
